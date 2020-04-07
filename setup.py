b1559cae900aa5e9b908f93451e0be4fe4ef144c
Stefan Daehling <SDaehling@eonerc.rwth-aachen.de>
2020-04-07 13:25:40 +0200

    add dependencies to setup

--- setup.py
+++ setup.py
@@ -9,4 +9,12 @@ setuptools.setup(
     url="https://git.rwth-aachen.de/acs/public/cloud/mas/cmapy",
     packages=setuptools.find_packages(),
     python_requires='>=3.5',
+    install_requires=[
+        'certifi==2019.11.28',
+        'chardet==3.0.4',
+        'idna==2.8',
+        'paho-mqtt==1.5.0',
+        'requests==2.22.0',
+        'urllib3==1.25.8'
+    ],
 )

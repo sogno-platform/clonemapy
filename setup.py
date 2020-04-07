23fa92a8f3a9d8a2a5e11fd41efc916dc968b794
Stefan Daehling <SDaehling@eonerc.rwth-aachen.de>
2020-04-07 11:28:29 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="cmapy",
+    version="0.0.1",
+    author="Stefan Dähling",
+    author_email="SDaehling@eonerc.rwth-aachen.de",
+    description="Python package for cloneMAP agents",
+    url="https://git.rwth-aachen.de/acs/public/cloud/mas/cmapy",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+)
